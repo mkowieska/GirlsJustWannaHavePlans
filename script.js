d81e5a74fe78a53b9c0d572e08c589a9fe2130a4
Emilia Lewandowska <emilcialewandowska@gmail.com>
2025-01-02 14:29:12 +0100

    frontend

---
+++ script.js
@@ -0,0 +1,5 @@
+function Filters() {
+    const filterOptions = document.querySelector('.filter-options');
+    filterOptions.style.display = filterOptions.style.display === 'block' ? 'none' : 'block';
+  }
+  
